627ec95890f8e09588b24f4a3ab029bca6a2512a
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-01-04 06:24:13 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+end
